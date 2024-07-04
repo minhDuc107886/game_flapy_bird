c41c6fcc01cfcbca9dea5b16d5d89124aeeae8c4
minhDuc107886 <minhduc107886@gmail.com>
2024-07-04 15:44:40 +0700

    change path of image

--- lib/game/assets.dart
+++ lib/game/assets.dart
@@ -1,16 +1,16 @@
 class Assets {
-  static const background = "assets/images/background.png";
-  static const ground = "assets/images/ground.png";
-  static const clouds = "assets/images/clouds.png";
-  static const pipe = "assets/images/pipe.png";
-  static const pipeRotated = "assets/images/pipe_rotated.png";
+  static const background = "background.png";
+  static const ground = "ground.png";
+  static const clouds = "clouds.png";
+  static const pipe = "pipe.png";
+  static const pipeRotated = "pipe_rotated.png";
 
-  static const birdMidFlap = "assets/images/bird_midflap.png";
-  static const birdUpFlap = "assets/images/bird_upflap.png";
-  static const birdDownFlap = "assets/images/bird_downflap.png";
+  static const birdMidFlap = "bird_midflap.png";
+  static const birdUpFlap = "bird_upflap.png";
+  static const birdDownFlap = "bird_downflap.png";
 
-  static const gameOver = "assets/images/gameover.png";
-  static const menu = "assets/images/menu.png";
-  static const message = "assets/images/message.png";
+  static const gameOver = "gameover.png";
+  static const menu = "menu.png";
+  static const message = "message.png";
 
 }
