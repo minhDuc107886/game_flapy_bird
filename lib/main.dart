fa13a27c9583fa322ed46b238ccbf2e4632d8326
minhDuc107886 <minhduc107886@gmail.com>
2024-07-04 14:00:03 +0700

    create project and add image

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'game/flappy_bird_game.dart';
+
+void main() {
+  final game = FlappyBirdGame();
+  runApp(
+    GameWidget(game: game),
+  );
+}
+
