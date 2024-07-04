b3fb3c354d7d482e9e7c976fa8a1156faf75a555
minhDuc107886 <minhduc107886@gmail.com>
2024-07-04 17:08:07 +0700

    complete ground

---
+++ lib/components/ground.dart
@@ -0,0 +1,28 @@
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/parallax.dart';
+import 'package:flappy_bird/game/flappy_bird_game.dart';
+
+import '../game/assets.dart';
+import '../game/configuration.dart';
+
+class Ground extends ParallaxComponent<FlappyBirdGame> {
+
+  Ground();
+
+  @override
+  Future<void> onLoad() async {
+    final ground = await Flame.images.load(Assets.ground);
+    parallax = Parallax([
+      ParallaxLayer(
+        ParallaxImage(ground, fill: LayerFill.none)
+      )
+    ]);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    parallax?.baseVelocity.x = Config.gameSpeed;
+  }
+}
