205e6dc7241f23f25d6e924d5b617a1aa632db3c
minhDuc107886 <minhduc107886@gmail.com>
2024-07-04 17:09:14 +0700

    create parallax with ground and back ground

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,13 +1,18 @@
 import 'dart:ui';
 
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 import '../components/background.dart';
+import '../components/ground.dart';
 
 class FlappyBirdGame extends FlameGame{
   @override
   Future<void> onLoad() async {
-    add(Background());
+    addAll([
+      Background(),
+      Ground(),
+    ]);
   }
 }
