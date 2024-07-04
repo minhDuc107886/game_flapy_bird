bde03e39117a9f597cd27316d0af1a902eb042f0
minhDuc107886 <minhduc107886@gmail.com>
2024-07-04 14:20:53 +0700

    add audio file

---
+++ lib/game/assets.dart
@@ -0,0 +1,16 @@
+class Assets {
+  static const background = "assets/images/background.png";
+  static const ground = "assets/images/ground.png";
+  static const clouds = "assets/images/clouds.png";
+  static const pipe = "assets/images/pipe.png";
+  static const pipeRotated = "assets/images/pipe_rotated.png";
+
+  static const birdMidFlap = "assets/images/bird_midflap.png";
+  static const birdUpFlap = "assets/images/bird_upflap.png";
+  static const birdDownFlap = "assets/images/bird_downflap.png";
+
+  static const gameOver = "assets/images/gameover.png";
+  static const menu = "assets/images/menu.png";
+  static const message = "assets/images/message.png";
+
+}
