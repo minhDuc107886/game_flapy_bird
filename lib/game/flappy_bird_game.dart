e297818b924c932361be903d1d0f96f7128f13f6
minhDuc107886 <minhduc107886@gmail.com>
2024-07-04 15:46:23 +0700

    load back ground

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,5 +1,13 @@
+import 'dart:ui';
+
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import '../components/background.dart';
 
 class FlappyBirdGame extends FlameGame{
-
+  @override
+  Future<void> onLoad() async {
+    add(Background());
+  }
 }
