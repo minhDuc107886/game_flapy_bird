67022ca3493b0e4957b6869127545bf888755619
minhDuc107886 <minhduc107886@gmail.com>
2024-07-05 17:00:40 +0700

    add and create bird in game

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -5,14 +5,17 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 import '../components/background.dart';
+import '../components/bird.dart';
 import '../components/ground.dart';
 
 class FlappyBirdGame extends FlameGame{
+  late Bird bird;
   @override
   Future<void> onLoad() async {
     addAll([
       Background(),
       Ground(),
+      bird = Bird(),
     ]);
   }
 }
