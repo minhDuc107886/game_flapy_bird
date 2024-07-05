394c976d9d6d682f094837cd575e314dff9a61ec
minhDuc107886 <minhduc107886@gmail.com>
2024-07-05 16:44:27 +0700

    add bird and bird movement file

---
+++ lib/components/bird.dart
@@ -0,0 +1,26 @@
+import 'package:flame/components.dart';
+import 'package:flappy_bird/game/bird_movement.dart';
+import 'package:flappy_bird/game/flappy_bird_game.dart';
+
+import '../game/assets.dart';
+
+class Bird extends SpriteGroupComponent<BirdMovement>
+    with HasGameRef<FlappyBirdGame>{
+  Bird();
+
+  @override
+  Future<void> onLoad() async{
+    final birdMidFlap = await gameRef.loadSprite(Assets.birdMidFlap);
+    final birdUpFlap = await gameRef.loadSprite(Assets.birdUpFlap);
+    final birdDownFlap = await gameRef.loadSprite(Assets.birdDownFlap);
+    size = Vector2(50, 40);
+
+    current = BirdMovement.midle;
+    sprites = {
+      BirdMovement.midle: birdDownFlap,
+      BirdMovement.up : birdUpFlap,
+      BirdMovement.down: birdDownFlap
+    };
+
+  }
+}
