d144d5e27ecd3537a0753b6fb48f7b764a6b3ac7
minhDuc107886 <minhduc107886@gmail.com>
2024-07-06 18:34:22 +0700

    add pipe to game

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,21 +1,36 @@
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flappy_bird/components/pip_group.dart';
+import 'package:flappy_bird/components/pipe.dart';
 import 'package:flutter/material.dart';
 
 import '../components/background.dart';
 import '../components/bird.dart';
 import '../components/ground.dart';
+import 'configuration.dart';
 
 class FlappyBirdGame extends FlameGame{
   late Bird bird;
+  Timer interval = Timer(Config.pipeInterval, repeat: true);
+
   @override
   Future<void> onLoad() async {
     addAll([
       Background(),
       Ground(),
       bird = Bird(),
+      PipeGroup(),
     ]);
+
+    interval.onTick = () => add(PipeGroup());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    interval.update(dt);
   }
 }
